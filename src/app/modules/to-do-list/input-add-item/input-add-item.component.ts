acd650a43276dc407f71a8e08831a3a9a86ca3d7
Luiz Carlos <121176269+LcsfCarlos@users.noreply.github.com>
2024-11-14 10:18:30 +0000

    Projeto concluido

---
+++ src/app/modules/to-do-list/input-add-item/input-add-item.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, Output, output, ViewChild, viewChild } from '@angular/core';
+
+//Interface
+import { JsonPipe, NgClass } from '@angular/common';
+import { IListItems } from '../interface/IListItems.interface';
+
+@Component({
+  selector: 'app-input-add-item',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './input-add-item.component.html',
+  styleUrl: './input-add-item.component.scss'
+})
+export class InputAddItemComponent {
+    #cdr = inject(ChangeDetectorRef);
+
+    @ViewChild("inputText") public inputText!: ElementRef; 
+
+    @Input({required: true}) public inputListItems: IListItems[] = []
+    @Output() public outputAddListItem = new EventEmitter<IListItems>();
+
+    public focusAndAddItem(value: string){
+      if(value){
+        this.#cdr.detectChanges();
+        this.inputText.nativeElement.value = '';
+
+        const currentDate = new Date();
+        const timestamp = currentDate.getTime();
+        const id = `ID ${timestamp}`
+
+        this.outputAddListItem.emit({
+          id, 
+          checked: false,
+          value
+        })
+
+        return this.inputText.nativeElement.focus();
+      }
+    }
+}
